94ed5c20545174d01aa183e1575524857e468beb
Relk <relk@maicoin.com>
2021-04-16 22:58:23 +0800

    helmfile autocomplete

---
+++ READMD.md
@@ -0,0 +1,17 @@
+# zsh-helmfile
+
+## Installation
+
+### Oh My Zsh
+
+1. Clone this repository into `$ZSH_CUSTOM/plugins` (by default `~/.oh-my-zsh/custom/plugins`)
+
+    ```sh
+    git clone https://github.com/Downager/zsh-helmfile ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-helmfile
+    ```
+
+2. Add the plugin to the list of plugins for Oh My Zsh to load (inside `~/.zshrc`):
+
+    ```sh
+    plugins=(zsh-helmfile)
+    ```
---
+++ zsh-helmfile.plugin.zsh
@@ -0,0 +1,11 @@
+if (( $+commands[kubectl] )); then
+    __HELMFILE_COMPLETION_FILE="${ZSH_CACHE_DIR}/HELMFILE_completion"
+
+    if [[ ! -f $__HELMFILE_COMPLETION_FILE || ! -s $__HELMFILE_COMPLETION_FILE ]]; then
+        curl https://raw.githubusercontent.com/roboll/helmfile/master/autocomplete/helmfile_zsh_autocomplete >! $__HELMFILE_COMPLETION_FILE
+    fi
+
+    [[ -f $__HELMFILE_COMPLETION_FILE ]] && source $__HELMFILE_COMPLETION_FILE
+
+    unset __HELMFILE_COMPLETION_FILE
+fi
